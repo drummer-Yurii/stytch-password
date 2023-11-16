71da2316ef6f279a7ce584a881c779404349bfd6
Yurii Rybachok <drummer2009@ukr.net>
2023-11-16 11:28:23 +0200

    create home page with logout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <template>
     <main>
         <h1>Hello, World!</h1>
+
+        <button>Logout</button>
     </main>
 </template>
+
+<style scoped>
+main {
+    padding: 1.5rem;
+}
+
+h1 {
+    margin-bottom: 1rem;
+}
+</style>
